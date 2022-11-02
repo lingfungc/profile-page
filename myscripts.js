a8fc2a3a74b047acfe946f501a1c03eeb024f752
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-02 22:46:43 +0000

    completed basic carousel function

--- myscripts.js
+++ myscripts.js
@@ -1,33 +1,4 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-console.log("hello, world")
+console.log("hello, javascript")
 
 const buttons = document.querySelectorAll("[data-carousel-button]");
 // const buttons = document.querySelectorAll(".carousel-button");
@@ -37,20 +8,14 @@ buttons.forEach(button => {
     console.log("hello, javascript");
 
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel").querySelector("[data-slides]")
+    const slides = button.closest("[data-carousel").querySelector("[data-slides]");
 
     const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].infexOf(activeSlide) + offset;
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
 
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active
   })
-})
-
-const check = document.querySelector("#profile-picture");
-
-check.addEventListener("click", () => {
-  console.log("hello, javascript");
 })
