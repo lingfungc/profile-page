2de5c5b295bfc0b1feef7649a78caba71dda194a
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-02 17:42:03 +0000

    updated carousel javascript

--- myscripts.js
+++ myscripts.js
@@ -28,10 +28,16 @@ function showSlides(n) {
 }
 
 
+
 const buttons = document.querySelector('[data-carousel-button]')
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button.closest("[data-carousel").querySelector([data-slides])
 
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].infexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
   })
 })
