0018ea934516884a65e3b3b2c6e17afde7fd4243
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-02 22:23:08 +0000

    defered loading javascript in html

--- myscripts.js
+++ myscripts.js
@@ -1,43 +1,56 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
 
+console.log("hello, world")
 
-
-const buttons = document.querySelector('[data-carousel-button]')
+const buttons = document.querySelectorAll("[data-carousel-button]");
+// const buttons = document.querySelectorAll(".carousel-button");
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
+    console.log("hello, javascript");
+
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel").querySelector([data-slides])
+    const slides = button.closest("[data-carousel").querySelector("[data-slides]")
 
     const activeSlide = slides.querySelector("[data-active]");
     let newIndex = [...slides.children].infexOf(activeSlide) + offset;
     if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active
   })
+})
+
+const check = document.querySelector("#profile-picture");
+
+check.addEventListener("click", () => {
+  console.log("hello, javascript");
 })
